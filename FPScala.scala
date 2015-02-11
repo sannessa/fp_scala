55a2b7ac7f78d1d2813ce6cf2d37805a516c53f3
Steve Annessa <steve.annessa@ziplist.com>
2015-02-10 22:18:27 -0500

    Though I solved 2.3 correctly, I'm not sure I fully understand the intention

--- FPScala.scala
+++ FPScala.scala
@@ -1,4 +1,5 @@
 object FPScala {
+  // 2.1
   def fibonnaci(n: Int): Int = {
     @annotation.tailrec
     def go(n: Int, curr: Int, prev: Int, prevPrev: Int): Int = {
@@ -11,6 +12,7 @@ object FPScala {
     else go(n, 2, 1, 0)
   }
 
+  // 2.2
   def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
     @annotation.tailrec
     def go(n: Int): Boolean = {
@@ -21,8 +23,13 @@ object FPScala {
 
     go(0)
   }
-
+ 
   def aLessThanB(a: Int, b: Int): Boolean = {
     a < b
+  }
+
+  // 2.3
+  def currying[A,B,C](f: (A,B) => C): A => (B => C) = {
+    (a: A) => (b: B) => f(a,b)
   }
 }
